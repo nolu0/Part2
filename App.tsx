97ad527c91688fa164cd2124dd8b29eb1808c11a
nolu0 <st10457711@vcconnect.edu.za>
2025-10-22 08:24:57 +0200

    third commit

--- App.tsx
+++ App.tsx
@@ -6,23 +6,29 @@ import MenuScreen from './MenuScreen';
 import PaymentScreen from './PaymentScreen';
 import { RootStackParamList } from './type';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: { backgroundColor: '#ff884d' },
-          headerTintColor: '#fff',
-          headerTitleStyle: { fontWeight: 'bold' },
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Menu" component={MenuScreen} />
-        <Stack.Screen name="Payment" component={PaymentScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen 
+          name="Menu" 
+          component={MenuScreen} 
+          options={{ title: 'Manage Menu' }}
+        />
+        <Stack.Screen 
+          name="Payment" 
+          component={PaymentScreen} 
+          options={{ title: 'Payment' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
